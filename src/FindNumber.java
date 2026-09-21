e58284827e4a8336d076479f17f080005017ad2e
Nicolaj Lydeking <nicolajlydeking@gmail.com>
2026-09-21 14:45:02 +0200

    Løst opgave for DuplicateCondition.java

--- src/FindNumber.java
+++ src/FindNumber.java
@@ -4,8 +4,8 @@ void main() {
     int[] numbers = {4, 8, 15, 16, 23, 42};
     int target = 15;
 
-    for (int number : numbers) {
-        if (number != target) {
+    for (int scannedNumber : numbers) {
+        if (scannedNumber == target) {
             IO.println("Found");
         } else {
             IO.println("Not found");
