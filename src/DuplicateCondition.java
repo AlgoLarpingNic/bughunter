fdd037d559431d69685ce708afbd3c4ae26d4d10
Nicolaj Lydeking <nicolajlydeking@gmail.com>
2026-09-21 14:37:28 +0200

    Løst opgave for DuplicateCondition.java

--- src/DuplicateCondition.java
+++ src/DuplicateCondition.java
@@ -2,16 +2,7 @@
 
 void main() {
     int age = 25;
-
     if (age >= 18) {
-        IO.println("Can vote");
-    }
-
-    if (age >= 18) {
-        IO.println("Can drive");
-    }
-
-    if (age >= 18) {
-        IO.println("Is an adult");
+        IO.println("Is an adult, can drive and can vote");
     }
 }
