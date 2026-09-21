d0a7b3f15516a9e25309cc47c2bfc37a02abd562
Nicolaj Lydeking <nicolajlydeking@gmail.com>
2026-09-21 14:27:12 +0200

    Løst opgaverne for Average, BankAccount og CheckPositive

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,17 +1,32 @@
 /* Skriv en klasse til at håndtere bankkonti */
 
-double balance = 1000;
+public class BankAccounting{
+    private int id;
+    private double balance;
+    private String name;
 
+    public BankAccounting(int id, double balance, String name){
+        this.id = id;
+        this.balance = balance;
+        this.name = name;
+    }
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+}
 void main() {
-    withdraw(1200);
-    deposit(100);
-    IO.println(balance);
+    BankAccounting bankAccount1 = new BankAccounting (1, 1000, "Andreas");
+    bankAccount1.withdraw(1200);
+    bankAccount1.deposit(100);
+    IO.println(bankAccount1.balance);
+    if (bankAccount1.balance < 0){
+        IO.println("Du er i overtræk med: " + bankAccount1.balance);
+
+    }
 }
 
-void withdraw(double amount) {
-    balance = balance - amount;
-}
-
-void deposit(double amount) {
-    balance = balance + amount;
-}
